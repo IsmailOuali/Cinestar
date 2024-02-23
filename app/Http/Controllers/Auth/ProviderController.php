66ea0e24e4d0278e2d24f2ff4536c0da1b3fd85c
unknown <ismail.ouali@ump.ac.ma>
2024-02-23 10:07:03 +0100

    Trying to fix the log with google (Driver not found)

--- app/Http/Controllers/Auth/ProviderController.php
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
 
 
 class ProviderController extends Controller
@@ -14,7 +18,20 @@ class ProviderController extends Controller
     }
 
     public function callback($provider){
-        $user = Socialite::driver($provider)->user();
-        dd($user);
+        $socialUser = Socialite::driver($provider)->user();
+ 
+        $user = User::updateOrCreate([
+            'provider_id' => $socialUser->id,
+            'provider' => $provider
+        ], [
+            'name' => $socialUser->name,
+            'email' => $socialUser->email,
+            'provider_token' => $socialUser->token,
+            'provider_refresh_token' => $socialUser->refreshToken,
+        ]);
+     
+        Auth::login($user);
+
+        return redirect('dashboard');
     }
 }
